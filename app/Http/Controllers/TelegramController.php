f140044b0d4c6d6b8fd9e600ce44f66faf79bbc3
Iván Alejandro Díaz Acevedo <ivan.diaza26@anahuac.mx>
2017-04-07 01:31:24 -0500

    Anexo de menu de opciones

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -19,6 +19,52 @@ class TelegramController extends Controller
         dd($updates);
     }
 
+    public function newMessage(Request $request){
+        $entrada = $request->all();
+        $mensaje = $entrada['message']['text'];
+        $chatID = $entrada['message']['chat']['id'];
+        $respuesta;
+
+        switch ($mensaje) {
+            case "/start":
+                $respuesta="Te damos la bienvenida, gracias por usar nuestro bot universitario\n\n";
+                $respuesta.="===Menú de ayuda\n\n";
+                $respuesta.= "Si quieres saber la clave del internet escribe o dale clic a /internet\n\n";
+                $respuesta.= "Si quieres saber la clave de los candados escribe o dale clic a /candado\n\n";
+                $respuesta.= "Si necesita ayuda personalizada, estamos ubicados al lado de la máquina de dulces en el edificio A\n";
+                $respuesta.= "Sino estuvieramos ahí entonces escribe o dale clic a /asistencia\n";
+                break;
+            case "/ayuda":
+                $respuesta="===Menú de ayuda\n\n";
+                $respuesta.= "Si quieres saber la clave del internet escribe o dale clic a /internet\n\n";
+                $respuesta.= "Si quieres saber la clave de los candados escribe o dale clic a /candado\n\n";
+                $respuesta.= "Si necesita ayuda personalizada, estamos ubicados al lado de la máquina de dulces en el edificio A\n";
+                $respuesta.= "Sino estuvieramos ahí entonces escribe o dale clic a /asistencia\n";
+                break;
+            case "/internet":
+                $respuesta = "La clave del internet es SomosAnahuac";
+                break;
+            case "/candado":
+                $respuesta = "La clave del candado es 460";
+                break;
+            case "/asistencia":
+                $nombre = $entrada['message']['chat']['first_name'];
+                $respuesta = "Hemos recibido tu solicitud y nos pondremos en contacto contigo lo más pronto posible ".$nombre;
+                break;
+            default:
+                $respuesta= "Por favor tiene que seleccionar una de las siguientes opciones\n\n";
+                $respuesta.= "Si quieres saber la clave del internet escribe o dale clic a /internet\n\n";
+                $respuesta.= "Si quieres saber la clave de los candados escribe o dale clic a /candado\n\n";
+                $respuesta.= "Si necesita ayuda personalizada, estamos ubicados al lado de la máquina de dulces en el edificio A\n";
+                $respuesta.= "Sino estuvieramos ahí entonces escribe o dale clic a /asistencia\n";
+        }
+
+        Telegram::sendMessage([
+            'chat_id' => $chatID,
+            'text' =>  $respuesta
+        ]);
+    }
+
     public function getMe()
     {
         $response = Telegram::getMe();
